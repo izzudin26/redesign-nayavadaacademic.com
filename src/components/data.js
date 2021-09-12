3d0f4b7deaca0c0e491040a014d7d540a6c8ad85
izzudin26 <izzudin.ar26@gmail.com>
2021-09-13 06:59:49 +0700

    add social and kursus data

--- src/components/data.js
+++ src/components/data.js
@@ -26,7 +26,7 @@ export const paketKursus = [
   {
     type: "Les Private",
     name: "klasifikasi data di machine learning python",
-    price: 200,
+    price: 250,
     materi: [
       "Support Vector Machine",
       "Naive Bayes",
@@ -91,5 +91,51 @@ export const paketKursus = [
       "4x Pertemuan",
       "90 Menit Setiap Pertemuan",
     ],
+  },
+];
+
+export const social = [
+  {
+    type: "github",
+    label: "nurnaff",
+    style: "text-black hover:border-black",
+    icon: require("../assets/github.png"),
+    direct: "https://github.com/nurnaff",
+  },
+  {
+    type: "youtube",
+    label: "Nur Nafi'iyah",
+    style: "text-red-500 hover:border-red-500",
+    icon: require("../assets/youtube.png"),
+    direct: "https://www.youtube.com/channel/UCHxYtQ5TpBfLQarIotL_uJQ",
+  },
+  {
+    type: "gmail",
+    label: "mynaff26@gmail.com",
+    style: "text-red-500 hover:border-red-500",
+    icon: require("../assets/gmail.png"),
+    direct: "mailto:mynaff26@gmail.com",
+  },
+  {
+    type: "whatsapp",
+    label: "Whatsapp",
+    style: "text-green-500 hover:border-green-500",
+    icon: require("../assets/whatsapp.png"),
+    direct:
+      "https://wa.me/6285649980150?text=assalamualaikum%20wr.wb%20saya%20tertarik%20dengan%20les%20private%20nayavadaacademic.com",
+  },
+  {
+    type: "telegram",
+    label: "Telegram",
+    style: "text-blue-400 hover:border-blue-400",
+    icon: require("../assets/telegram.png"),
+    direct: "https://t.me/NurNafiiyah",
+  },
+  {
+    type: "Facebook",
+    label: "Facebook",
+    style: "text-blue-600 hover:border-blue-600",
+    icon: require("../assets/facebook.png"),
+    direct: "https://www.facebook.com/nurnafiiyah",
   },
 ];
