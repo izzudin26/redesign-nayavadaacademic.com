fb4a0d74a6c1aa16557b13debe8c9940cdfe1915
izzudin26 <izzudin.ar26@gmail.com>
2021-09-13 07:00:19 +0700

    merge all components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
-  <div id="app">
+  <div id="app" class="transform transition-all duration-1000">
+    <Header />
     <Main />
     <PaketKursus />
+    <Social />
+    <Footer />
   </div>
 </template>
 
 <script>
+import Header from "./components/header.vue";
 import Main from "./components/main.vue";
 import PaketKursus from "./components/packet.vue";
+import Social from "./components/social.vue";
+import Footer from "./components/footer.vue";
 
 export default {
   name: "App",
   components: {
+    Header,
     Main,
     PaketKursus,
+    Social,
+    Footer,
   },
 };
 </script>
